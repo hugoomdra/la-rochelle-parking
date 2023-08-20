8a766f61714d248ce0cc7d593cbfc8586d5c9fce
hugoomdra <hugoomdra@gmail.com>
2023-08-20 19:49:45 +0200

    change style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,23 +13,22 @@ async function getData() {
   return res.json()
 }
 
-
-
 export default async function Home() {
-
 
   const data = await getData();
 
   console.log(data);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between gap-8 px-8 py-16 lg:w-1/3 mx-auto">
+      <p className='text-black text-2xl text-center'>Listes des parkings de <br /><span className='font-bold'>La Rochelle</span></p>
       {data.records.map((record : any) => (
-        <div key={record.recordid} className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold">{record.fields.nom}</h1>
-          <h2 className="text-2xl font-bold">{record.fields.nb_places_disponibles} / {record.fields.nb_places}</h2>
+        <div key={record.recordid} className="flex flex-col items-center justify-center text-black border-2 border-black rounded-lg h-48 w-full px-4 text-center">
+          <p className="text-lg font-bold">{record.fields.nom}</p>
+          <p className="text-xl">{record.fields.nb_places_disponibles} / {record.fields.nb_places}</p>
         </div>
       ))}
+      <p className='text-black'>Made with ❤️ by <a href="" className='underline'>HugooMdra</a></p>
     </main>
   )
 }
